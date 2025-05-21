e237d35e79b003f1ac454875932fde55496be91a
Edgar <edwin.wesatu@gmail.com>
2025-05-21 13:59:46 +0700

    Updateterbaru

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 // script.js
+
 const toggleBtn = document.getElementById('menu-toggle');
 const nav = document.querySelector('nav');
 
 toggleBtn.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+// Fungsi untuk menampilkan dan menyembunyikan detail dalam box akademik
+function toggleDetail(id) {
+  const detail = document.getElementById(id);
+  if (detail.style.display === "none" || detail.style.display === "") {
+    detail.style.display = "block";
+  } else {
+    detail.style.display = "none";
+  }
+}
